6db08c3af447114b06b92a462438dd83797b9939
Madhav <madhav.adiki@gmail.com>
2025-09-06 23:51:31 +0530

    testing

--- src/main/java/com/example/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//this is application class
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
